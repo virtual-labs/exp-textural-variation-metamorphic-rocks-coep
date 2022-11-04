a492246e82e2edc9a5ad03c27a9afacbbb4c4f16
COEP Virtual Lab <coevlab@gmail.com>
2022-11-04 11:28:22 +0530

    Update metaRocksJson.js

--- experiment/simulation/src/metaRocksJson.js
+++ experiment/simulation/src/metaRocksJson.js
@@ -155,77 +155,35 @@ tempMasterJson.demo = masterJsonArr;
 //Fourth metaRock
 tempQuestionArr = [];
 questionAnswer = {
-	"question": "What is the chemical classification of this metamorphic rock?",
-	"correctAns": "Medium- to high-grade foliated metamorphic rock.",
-	"wrongAns1": "Carbonite",
-	"wrongAns2": "Banded Haematite Quartzite",
-	"wrongAns3": "Fe Ore"
+	"question": "Slaty cleavage is result of which metamorphism of clays and shales?",
+	"correctAns": "Dynamic",
+	"wrongAns1": "Thermal",
+	"wrongAns2": "Phyllosilicate",
+	"wrongAns3": "Barometric"
 };
 tempQuestionArr.push(questionAnswer);
 questionAnswer = {
-	"question": "What is the color of this metamorphic rock?",
-	"wrongAns1": "White",
-	"correctAns": "Black, dark green, dark brown",
-	"wrongAns2": " Brown",
-	"wrongAns3": "Variable"
+	"question": "Schistose is resulted due to __________",
+	"correctAns": "Recrystallization and reorientation",
+	"wrongAns1": "Reforming",
+	"wrongAns2": "Recrystallization",
+	"wrongAns3": "Reorientation"
 };
 tempQuestionArr.push(questionAnswer);
 questionAnswer = {
-	"question": "What is the streak property of this metamorphic rock?",
-	"wrongAns1": "Red to Reddish Brown ",
-	"correctAns": "White and occasionally greyish flakes",
-	"wrongAns2": "Black",
-	"wrongAns3": "White"
+	"question": "Pick the characteristic of gneiss which is wrong",
+	"wrongAns1": "Gneiss does not take polish",
+	"correctAns": "Gneiss also comes in banded structure or texture",
+	"wrongAns2": "Pink gneiss is also observed at many places",
+	"wrongAns3": "Gneiss takes polish well"
 };
 tempQuestionArr.push(questionAnswer);
 questionAnswer = {
-	"question": "What is the luster property of this metamorphic rock?",
-	"wrongAns1": "Dull",
-	"correctAns": "Pearly",
-	"wrongAns2": "Shiny",
-	"wrongAns3": "Metallic, earthy"
-};
-tempQuestionArr.push(questionAnswer);
-questionAnswer = {
-	"question": "What is the cleavage of this metamorphic rock?",
-	"correctAns": "Perfect",
-	"wrongAns1": "None",
-	"wrongAns2": "Rhombohedral cleavage",
-	"wrongAns3": "Poor"
-};
-tempQuestionArr.push(questionAnswer);
-questionAnswer = {
-	"question": " What is the Mohs hardness of this metamorphic rock?",
-	"correctAns": "2.5-3	",
-	"wrongAns1": "3.5-4",
-	"wrongAns2": "6.5-5",
-	"wrongAns3": "5.5"
-};
-tempQuestionArr.push(questionAnswer);
-questionAnswer = {
-	"question": " What is the specific gravity of this metamorphic rock?",
-	"correctAns": "2.7-3.4",
-	"wrongAns1": "4.3-5",
-	"wrongAns2": "2.2-2.9",
-	"wrongAns3": "3.5-2"
-};
-tempQuestionArr.push(questionAnswer);
-
-questionAnswer = {
-	"question": " What is the Chemical composition of this metamorphic rock?",
-	"correctAns": "Platy minerals such as muscovite, chlorite, talc, sericite, biotite, and graphite	",
-	"wrongAns1": "BaSO<sub>4</sub>Mg<sub>4.5-2.3</sub>Fe<sup>2+</sup><sub>0.5-2.5</sub>",
-	"wrongAns2": "SiO<sub>2</sub>Cu(OH)<sub>2</sub>",
-	"wrongAns3": "It contains 38.9% Fe, 42.32% SiO<sub>2</sub> and 1.03% Al<sub>2</sub> O<sub>3</sub>"
-};
-tempQuestionArr.push(questionAnswer);
-
-questionAnswer = {
-	"question": " What is the diaphaneity of this metamorphic rock?",
-	"correctAns": "Transparent to translucent to opaque",
-	"wrongAns1": "None of these",
-	"wrongAns2": "Opaque to translucent",
-	"wrongAns3": "Transparent to translucent"
+	"question": "How many types on the basis of depth of focus are present",
+	"wrongAns1": "4",
+	"correctAns": "3",
+	"wrongAns2": "2",
+	"wrongAns3": "1"
 };
 tempQuestionArr.push(questionAnswer);
 
