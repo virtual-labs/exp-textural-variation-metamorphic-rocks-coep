daa8ca07d5f7759fca6ad0980ec93aed5f4d896e
vleadadmin <admin@vlabs.ac.in>
2023-01-10 05:47:07 +0000

    https://virtual-labs.github.io/virtual-labs/exp-textural-variation-metamorphic-rocks-coep click on the link to test your code.

---
+++ simulation/src/metaRocksJson.js
@@ -0,0 +1,202 @@
+tempMasterJson = {};
+var masterJsonArr = [];
+var tempQuestionArr = [];
+masterJson = {};
+
+questionAnswer = {
+	"question": "Slaty cleavage is result of which metamorphism of clays and shales?",
+	"correctAns": "Dynamic",
+	"wrongAns1": "Thermal",
+	"wrongAns2": "Phyllosilicate",
+	"wrongAns3": "Barometric"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Schistose is resulted due to __________",
+	"correctAns": "Recrystallization and reorientation",
+	"wrongAns1": "Reforming",
+	"wrongAns2": "Recrystallization",
+	"wrongAns3": "Reorientation"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Pick the characteristic of gneiss which is wrong",
+	"wrongAns1": "Gneiss does not take polish",
+	"correctAns": "Gneiss also comes in banded structure or texture",
+	"wrongAns2": "Pink gneiss is also observed at many places",
+	"wrongAns3": "Gneiss takes polish well"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "How many types on the basis of depth of focus are present",
+	"wrongAns1": "4",
+	"correctAns": "3",
+	"wrongAns2": "2",
+	"wrongAns3": "1"
+};
+
+tempQuestionArr.push(questionAnswer);
+//console.log(tempQuestionArr);
+masterJson = {
+	"mineralId": 1,
+	"dummyName": "Texture1",
+	"textureName": "Granoblastic Texture",
+	"src": "images/GNEISS1.jpg",
+	"src1": "images/GNEISS2.jpg",
+	"src2": "images/GNEISS3.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+// Second metaRock
+tempQuestionArr = [];
+
+questionAnswer = {
+	"question": "Slaty cleavage is result of which metamorphism of clays and shales?",
+	"correctAns": "Dynamic",
+	"wrongAns1": "Thermal",
+	"wrongAns2": "Phyllosilicate",
+	"wrongAns3": "Barometric"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Schistose is resulted due to __________",
+	"correctAns": "Recrystallization and reorientation",
+	"wrongAns1": "Reforming",
+	"wrongAns2": "Recrystallization",
+	"wrongAns3": "Reorientation"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Pick the characteristic of gneiss which is wrong",
+	"correctAns": "Gneiss does not take polish",
+	"wrongAns1": "Gneiss also comes in banded structure or texture",
+	"wrongAns2": "Pink gneiss is also observed at many places",
+	"wrongAns3": "Gneiss takes polish well"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "How many types on the basis of depth of focus are present",
+	"wrongAns1": "4",
+	"correctAns": "3",
+	"wrongAns2": "2",
+	"wrongAns3": "1"
+};
+tempQuestionArr.push(questionAnswer);
+
+//console.log(tempQuestionArr);
+masterJson = {
+	"mineralId": 2,
+	"dummyName": "Texture2",
+	"textureName": "Genissose Texture",
+	"src": "images/GRANITE GNEISS1.jpg",
+	"src1": "images/AUGEN GNEISS 2.jpg",
+	"src2": "images/AUGEN GNEISS 3.jpg",
+
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+// Third metaRock
+
+tempQuestionArr = [];
+
+questionAnswer = {
+	"question": "Slaty cleavage is result of which metamorphism of clays and shales?",
+	"correctAns": "Dynamic",
+	"wrongAns1": "Thermal",
+	"wrongAns2": "Phyllosilicate",
+	"wrongAns3": "Barometric"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Schistose is resulted due to __________",
+	"correctAns": "Recrystallization and reorientation",
+	"wrongAns1": "Reforming",
+	"wrongAns2": "Recrystallization",
+	"wrongAns3": "Reorientation"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Pick the characteristic of gneiss which is wrong",
+	"correctAns": "Gneiss does not take polish",
+	"wrongAns1": "Gneiss also comes in banded structure or texture",
+	"wrongAns2": "Pink gneiss is also observed at many places",
+	"wrongAns3": "Gneiss takes polish well"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "How many types on the basis of depth of focus are present",
+	"wrongAns1": "4",
+	"correctAns": "3",
+	"wrongAns2": "2",
+	"wrongAns3": "1"
+};
+tempQuestionArr.push(questionAnswer);
+
+//console.log(tempQuestionArr);
+masterJson = {
+	"mineralId": 3,
+	"dummyName": "Texture3",
+	"textureName": "Schistose Texture",
+	"src": "images/TALC ACTINOLITE SCHIST1.jpg",
+	"src1": "images/TALC ACTINOLITE SCHIST2.jpg",
+	"src2": "images/TALC ACTINOLITE SCHIST3.jpg",
+"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Fourth metaRock
+tempQuestionArr = [];
+questionAnswer = {
+	"question": "Slaty cleavage is result of which metamorphism of clays and shales?",
+	"correctAns": "Dynamic",
+	"wrongAns1": "Thermal",
+	"wrongAns2": "Phyllosilicate",
+	"wrongAns3": "Barometric"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Schistose is resulted due to __________",
+	"correctAns": "Recrystallization and reorientation",
+	"wrongAns1": "Reforming",
+	"wrongAns2": "Recrystallization",
+	"wrongAns3": "Reorientation"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "Pick the characteristic of gneiss which is wrong",
+	"wrongAns1": "Gneiss does not take polish",
+	"correctAns": "Gneiss also comes in banded structure or texture",
+	"wrongAns2": "Pink gneiss is also observed at many places",
+	"wrongAns3": "Gneiss takes polish well"
+};
+tempQuestionArr.push(questionAnswer);
+questionAnswer = {
+	"question": "How many types on the basis of depth of focus are present",
+	"wrongAns1": "4",
+	"correctAns": "3",
+	"wrongAns2": "2",
+	"wrongAns3": "1"
+};
+tempQuestionArr.push(questionAnswer);
+
+//console.log(tempQuestionArr);
+masterJson = {
+	"mineralId": 4,
+	"dummyName": "Texture4",
+	"textureName": "Slaty Texture",
+	"src": "images/SLATE 1.jpg",
+	"src1": "images/SLATE 2.jpg",
+	"src2": "images/SLATE 3.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
---
+++ theory.md
@@ -0,0 +1,26 @@
+#### Metamorphic Rocks:
+Metamorphic Rocks are formed through the transformation of the pre-existing rocks under increased temperature and pressure conditions. This process of transformation is known as Metamorphism. Formation of Metamorphism rock from a pre-existing is controlled by the following parameters Temperature, Pressure, Chemically active fluid these there are known as Metamorphism agents. Generally all these three acts together and cause Metamorphism. But sometimes, any one or two of them any dominate and play together and cause Metamorphism. But sometimes any one or two of them may dominate and play on active role.
+ #### Temperature: 
+ Changes take place in the temp 350-380˚C.
+ Pressure: Uniform pressure increase with depth direct pressure due to tectonic forces
+Chemically active fluids: Liquids which acts as carrier of chemical components, volatiles of magma and hydrothermal solutions.
+#### Texture
+Textures of metamorphic rocks fall into two broad groups, **FOLIATED and NON-FOLIATED.** Foliation is produced in a rock by the parallel alignment of platy minerals (e.g., muscovite, biotite, chlorite), needle-like minerals (e.g., hornblende), or tabular minerals (e.g., feldspars).
+**The differences in the orientation, or alignment, of crystals and the size of the crystals** determine the texture of a metamorphic rock. There are two general texture groups. Foliated textures are those in which platy or leaflike minerals such as mica or chlorite are nearly all aligned parallel to one another.<br>
+#### The five basic metamorphic textures with typical rock types are:
+- Slaty: slate and phyllite; the foliation is called 'slaty cleavage'
+- Schistose: schist; the foliation is called 'schistocity'
+- Gneissose: gneiss; the foliation is called 'gneisocity'
+- Granoblastic: granulite, some marbles and quartzite.<br>
+![meta1*************************************************](images/meta1.jpg)
+<br>**Quartzite** is a non-foliated metamorphic rock that is produced by the metamorphism of **sandstone**. It is composed primarily of **quartz**. The specimen above is about two inches (five centimeters) across.<br>
+![meta2*************************************************](images/meta2.jpg)
+<br>**Phyllite** is a foliated metamorphic rock that is made up mainly of very fine-grained mica. The surface of phyllite is typically lustrous and sometimes wrinkled. It is intermediate in grade between **slate** and **schist**. <br>
+![meta3*************************************************](images/meta3.jpg)<br>
+**Marble** is a non-foliated metamorphic rock that is produced from the metamorphism of **limestone** or **dolostone**. It is composed primarily of calcium carbonate. 
+<br>![meta4*************************************************](images/meta4.jpg)<br>
+Schist is a metamorphic rock with well-developed foliation. It often contains significant amounts of mica which allow the rock to split into thin pieces. It is a rock of intermediate metamorphic grade between phyllite and gneiss.
+<br>![meta5*************************************************](images/meta5.jpg)<br>
+**Amphibolite** is a non-foliated metamorphic rock that forms through recrystallization under conditions of high viscosity and directed pressure. It is composed primarily of **hornblende** (amphibole) and **plagioclase**, usually with very little **quartz**. 
+<br>![meta6*************************************************](images/meta6.jpg)<br>
+**Gneiss** is a foliated metamorphic rock that has a banded appearance and is made up of granular mineral grains. It typically contains abundant **quartz** or **feldspar** minerals. 
